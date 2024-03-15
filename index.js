80d01141dfb3aa7fa9e0f330150c12f8ff3be52b
AldoChagollan <aldochagollan@outlook.com>
2024-03-15 10:54:39 -0600

    to deploy

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require('dotenv').config();
 
+
 // Conexión a MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
